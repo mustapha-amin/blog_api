a1e6b5016a2850db10b64f3285a5fad3b8f74aac
Mustapha Amin <mustaphaamin2003@gmail.com>
2025-10-28 16:31:46 +0100

    feat: implement user controllers

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { updateUser, deleteUser, fetchUsers, fetchUserById } from "./user_controller.ts";
 
 const usersRouter = Router()
 
-usersRouter.post('/', () => { })
-usersRouter.patch('/:id', () => { })
-usersRouter.delete('/:id', () => { })
-usersRouter.get('/', () => { })
+usersRouter.patch('/:id', updateUser)
+usersRouter.delete('/:id', deleteUser)
+usersRouter.get('/', fetchUsers)
+usersRouter.get('/:id', fetchUserById)
 
 export default usersRouter
---
+++ src/modules/user/user_controller.ts
@@ -0,0 +1,50 @@
+import { type Request, type Response } from "express";
+import { BadRequestError, NotFoundError } from "../../model/api_error.ts";
+import { User } from "./user_model.ts";
+import { StatusCodes } from "http-status-codes";
+
+export async function updateUser(req: Request, res: Response) {
+	
+}
+
+export async function deleteUser(req: Request, res: Response) {
+	const id = req.params.id;
+	if (!id) {
+		throw new BadRequestError("Missing required parameters");
+	}
+
+	const deleted = await User.findByIdAndDelete(id);
+	if (!deleted) {
+		throw new NotFoundError("User not found");
+	}
+
+	return res.status(StatusCodes.OK).json({
+		message: "user deleted successfully"
+	})
+}
+
+export async function fetchUsers(_: Request, res: Response) {
+	const users = await User.find({}).select('email username role createdAt updatedAt');
+
+	return res.status(StatusCodes.OK).json({
+		message: "Users fetched successfully",
+		users: users
+	})
+}
+
+export async function fetchUserById(req: Request, res: Response) {
+    const id = req.params.id;
+    if(!id) {
+        throw new BadRequestError("Missing user id")
+    }
+
+    const user = await User.findById(id).select('email username role createdAt updatedAt');
+    if(!user) {
+        throw new NotFoundError("User not found")
+    }
+
+    return res.status(StatusCodes.OK).json({
+		message: "Users fetched successfully",
+		user
+	})
+}
